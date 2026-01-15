87c131829460986cc473271d7faef29644a02bb8
brandonchui <chuib@usc.edu>
2026-01-14 18:42:46 -0800

    cli refactor
    refactored to allow for better scalability

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+enum Command {
+    Repl,
+    RunFile(String),
+    Usage,
+}
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
+    match parse_args(&args) {
+        Command::Repl => run_prompt(),
+        Command::RunFile(path) => run_file(&path),
+        Command::Usage => println!("Usage: wren [script]"),
+    }
+}
+
+fn parse_args(args: &[String]) -> Command {
     if args.len() > 2 {
-        println!("Usage: wren [script]");
+        Command::Usage
     } else if args.len() == 1 {
-        // If we have no args given from the user, run the REPL
-        run_prompt();
+        Command::Repl
     } else {
-        run_file(&args[1]);
+        Command::RunFile(args[1].clone())
     }
 }
 
@@ -18,4 +31,27 @@ fn run_file(s: &str) {
 
 fn run_prompt() {
     //TODO
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_no_args_runs_repl() {
+        let args = vec!["wren".to_string()];
+        assert!(matches!(parse_args(&args), Command::Repl));
+    }
+
+    #[test]
+    fn test_one_arg_runs_file() {
+        let args = vec!["wren".to_string(), "script.wren".to_string()];
+        assert!(matches!(parse_args(&args), Command::RunFile(_)));
+    }
+
+    #[test]
+    fn test_too_many_args_shows_usage() {
+        let args = vec!["wren".to_string(), "a".to_string(), "b".to_string()];
+        assert!(matches!(parse_args(&args), Command::Usage));
+    }
 }
