6ce0ee758fe2d7ef94317b7dc915c1574abb60c5
brandonchui <chuib@usc.edu>
2026-01-14 17:34:53 -0800

    cli input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+
+    if args.len() > 2 {
+        println!("Usage: wren [script]");
+    } else if args.len() == 1 {
+        // If we have no args given from the user, run the REPL
+        run_prompt();
+    } else {
+        run_file(&args[1]);
+    }
+}
+
+fn run_file(s: &str) {
+    //TODO
+    println!("Running {s}");
+}
+
+fn run_prompt() {
+    //TODO
 }
