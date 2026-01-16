41e19ea949aec4f014256545cefc4cbd95216c7e
brandonchui <chuib@usc.edu>
2026-01-16 04:49:43 -0800

    ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,1 +1,19 @@
+use crate::token::{Literal, Token};
 
+pub enum Expr {
+    Binary {
+        left: Box<Expr>,
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Unary {
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Literal {
+        value: Literal,
+    },
+    Grouping {
+        expression: Box<Expr>,
+    },
+}
